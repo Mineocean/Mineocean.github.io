8641d19c1250a57e1622e33603185ecb38763c0b
Mineocean <Win2421@outlook.com>
2023-12-07 23:39:52 +0800

    Test
    First community

---
+++ valaxy.config.ts
@@ -0,0 +1,46 @@
+import { defineValaxyConfig } from 'valaxy'
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '清海的小站',
+      cloud: {
+        enable: true,
+      },
+    },
+
+    pages: [
+      {
+        name: '我的小伙伴们',
+        url: '/links/',
+        icon: 'i-ri-genderless-line',
+        color: 'dodgerblue',
+      },
+   
+    ],
+
+    footer: {
+      since: 2016,
+      beian: {
+        enable: true,
+        icp: 'Blog Dev Version',
+      },
+    },
+  },
+
+  unocss: { safelist },
+})
